a21a2cb77a56d5e77e6d44e6864bb9f2fe5c46a8
Victor Nunez <VN135766@gmail.com>
2022-05-02 17:56:18 -0400

    updated js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+    fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+        console.log("inside", response);
+    });
 
-getUserRepos();
+    console.log("outside");
+    getUserRepos();
